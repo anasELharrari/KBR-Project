f831e8f72c72e910d54cc610400ccc4c365fdb3d
ANAS EL HARRARI <76181279+anasELharrari@users.noreply.github.com>
2023-06-22 18:48:20 +0100

    Add files via upload

---
+++ actionspace.h
@@ -0,0 +1,20 @@
+#ifndef ACTIONSPACE_H
+#define ACTIONSPACE_H
+
+
+class ActionSpace
+{
+public:
+    ActionSpace();
+    addAction(int state, int action);
+    Action_decision(int state);
+public:
+   string CourseEnrolling;
+   string CourseRecommanding;
+   string ProgramAdjusting;
+   string ProgramSheduling;
+   string additional_resources_providing;
+
+};
+
+#endif // ACTIONSPACE_H
