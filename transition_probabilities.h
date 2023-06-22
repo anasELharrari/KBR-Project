6a742e48a9b5faeb038ab7ca2528906dc9113932
ANAS EL HARRARI <76181279+anasELharrari@users.noreply.github.com>
2023-06-22 18:50:21 +0100

    Add files via upload

---
+++ transition_probabilities.h
@@ -0,0 +1,11 @@
+#ifndef TRANSITION_PROBABILITIES_H
+#define TRANSITION_PROBABILITIES_H
+
+
+class transition_probabilities
+{
+public:
+    transition_probabilities();
+};
+
+#endif // TRANSITION_PROBABILITIES_H
